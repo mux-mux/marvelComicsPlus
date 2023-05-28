bdd67f737e38860c17d0668c99773bbd526fc8b5
mikemouse <kmmyemail@ukr.net>
2023-05-28 21:33:18 +0300

    creating http.hook

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,24 @@
+import { useState, useCallback } from 'react';
+export const useHttp = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const request = useCallback(
+    async (url, method = 'GET', body = null, headers = { 'Content-Type': 'application/json' }) => {
+      try {
+        const resp = await fetch(url, { method, body, headers });
+        if (!resp.ok) {
+          throw new Error(`Error ${url}, status: ${resp.status}`);
+        }
+        const data = await resp.json();
+        setLoading(false);
+        return data;
+      } catch (e) {
+        setLoading(false);
+        setError(e.message);
+        throw e;
+      }
+    },
+    []
+  );
+};
